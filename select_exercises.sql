9a78bbbd35ebde92686f732c0d8f82cb32ddb73f
Titus Williams <wtwilliams310@gmail.com>
2023-03-06 09:40:17 -0600

    completed Basics statements--select exercises

--- select_exercises.sql
+++ select_exercises.sql
@@ -8,8 +8,14 @@ describe albums;
 
 SELECT * FROM albums;
 
-SELECT 'Pink Floyd albums!' AS 'Info';
-SELECT * FROM albums WHERE artist = 'Pink Floyd';
+# SELECT 'Pink Floyd albums!' AS 'Info';
+# SELECT * FROM albums WHERE artist = 'Pink Floyd';
+#
+#
+# SELECT genre FROM albums WHERE name = 'Nevermind';
 
+# SELECT * FROM albums WHERE release_date BETWEEN 1990 and 1999;
 
-SELECT genre FROM albums WHERE name = 'Nevermind';
+# SELECT * FROM albums WHERE sales < 20;
+
+SELECT * FROM albums WHERE genre = 'Rock';
